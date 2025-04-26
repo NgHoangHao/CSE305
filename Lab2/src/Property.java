23461420e46b13f898809f409774b6dfa4c5b24a
NgHoangHao <haonguyenhoang8@gmail.com>
2025-04-26 09:13:53 +0700

    update

---
+++ Lab2/src/Property.java
@@ -0,0 +1,25 @@
+
+public class Property {
+    private String propertyID;
+    private String propertyDescription;
+    private String propertyOwnerID;
+    public String getPropertyID() {
+        return propertyID;
+    }
+    public String getPropertyDescription() {
+        return propertyDescription;
+    }
+    public String getPropertyOwnerID() {
+        return propertyOwnerID;
+    }
+    public void setPropertyID(String propertyID) {
+        this.propertyID = propertyID;
+    }
+    public void setPropertyDescription(String propertyDescription) {
+        this.propertyDescription = propertyDescription;
+    }
+    public void setPropertyOwnerID(String propertyOwnerID) {
+        this.propertyOwnerID = propertyOwnerID;
+    }
+    
+}
---
+++ Lab2/src/User.java
@@ -0,0 +1,38 @@
+
+public class User{
+    private String userID;
+    private String password;
+    private String email;
+    private String userType;
+    public String getUserID() {
+        return userID;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getUserType() {
+        return userType;
+    }
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+    public void createUser(){
+
+    }
+    public void resetPassWord(){
+
+    }
+
+}
