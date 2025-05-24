b55b78b8606fa683dd594b56693fdbea66e34629
NgHoangHao <haonguyenhoang8@gmail.com>
2025-05-24 09:46:13 +0700

    update
    update

--- Lab2/src/Tenant.java
+++ Lab2/src/Tenant.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Tenant {
     private User user;
-    private ArrayList<>listOfContract;
+    private ArrayList<RentalContracts>listOfContract;
     public void requestCreateRentalContract(){
 
     }
